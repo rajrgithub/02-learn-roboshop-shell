b4d3386252339998d5a3cc5f6246cbeaf1585592
brajasekharreddy <raj479.mits@gmail.com>
2023-01-06 03:17:43 +0530

    catalogue service

--- catalogue.sh
+++ catalogue.sh
@@ -1,6 +1,6 @@
 script_location=$(pwd)
 # set -e --- Command to to stop the script if there is any error
-set -e
+#set -e
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
@@ -21,4 +21,4 @@ systemctl start catalogue
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org-shell -y
 
-#mongo --host localhost </app/schema/catalogue.js
+mongo --host localhost </app/schema/catalogue.js
