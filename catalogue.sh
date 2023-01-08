322e15fd35515455763966b9913162829235644c
brajasekharreddy <raj479.mits@gmail.com>
2023-01-08 19:24:59 +0530

    if condition

--- catalogue.sh
+++ catalogue.sh
@@ -1,24 +1,127 @@
 script_location=$(pwd)
+LOG=/tmp/roboshop.log
+
 # set -e --- Command to to stop the script if there is any error
 #set -e
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
+echo -e "\e[35m Configuring NodeJS Repos \e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
+echo -e "\e[35m Install NodeJS  \e[0m"
+yum install nodejs -y &>>${LOG}
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
+echo -e "\e[35m Add Application User \e[0m"
+useradd roboshop &>>${LOG}
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
 # -p creates directory only if it doesn't exist and doesn't throw error
-mkdir -p /app
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-rm -rf /app/*
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
+echo -e "\e[35m Make Directory \e[0m"
+mkdir -p /app &>>${LOG}
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
+echo -e "\e[35m Downloading App content \e[0m"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${LOG}
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
+echo -e "\e[35m  Cleanup Old Content \e[0m"
+rm -rf /app/* &>>${LOG}
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
+echo -e "\e[35mExtracting App Content  \e[0m"
+cd /app &>>${LOG}
+unzip /tmp/catalogue.zip &>>${LOG}
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
+echo -e "\e[35m Installing NodeJS Dependencies \e[0m"
+cd /app &>>${LOG}
+npm install &>>${LOG}
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
+echo -e "\e[35m Configuring Catalogue Service File \e[0m"
 cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
+echo -e "\e[35m Reload SystemD \e[0m"
+systemctl daemon-reload &>>${LOG}
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
-mongo --host mongodb-dev.rajdevops.online </app/schema/catalogue.js
+echo -e "\e[35m Enable Catalogue Service \e[0m"
+systemctl enable catalogue &>>${LOG}
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
+echo -e "\e[35m  Start Catalogue service \e[0m"
+systemctl start catalogue &>>${LOG}
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
+echo -e "\e[35m Configuring Mongo Repo \e[0m"
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
+echo -e "\e[35m Install Mongo Client \e[0m"
+yum install mongodb-org-shell -y &>>${LOG}
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
+echo -e "\e[35m Load Schema \e[0m"
+mongo --host mongodb-dev.rajdevops.online </app/schema/catalogue.js &>>${LOG}
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
